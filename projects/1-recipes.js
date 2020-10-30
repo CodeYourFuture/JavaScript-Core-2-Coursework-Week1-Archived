3106aae6b1ae95d208d3612a26a5d66ec5b7e949
Chris Owen <cowen1992@hotmail.co.uk>
2020-10-30 14:08:27 +0000

    Add challenges

---
+++ projects/1-recipes.js
@@ -0,0 +1,25 @@
+/** 
+
+The Recipe Card
+Never forget another recipe!
+
+Create an object to hold information on your favorite recipe. 
+
+It should have properties for 
+
+- Title (a string), 
+- Servings (a number), and 
+- Ingredients (an array of strings)
+
+On separate lines (one console.log statement for each), log the recipe information so it looks like:
+
+Mole
+Serves: 2
+Ingredients:
+cinnamon
+cumin
+cocoa
+
+**/
+
+let recipes = {};
---
+++ projects/2-reading-list.js
@@ -0,0 +1,28 @@
+/**
+
+The Reading List
+Keep track of which books you read and which books you want to read!
+
+=====
+Exercise 1
+=====
+Create an array of objects, where each object describes a book and has properties for:
+
+- The title (a string)
+- Author (a string)
+- and alreadyRead (a boolean indicating if you read it yet)
+
+Loop through the array of books. For each book, log the book title and book author like so: 
+
+"The Hobbit by J.R.R. Tolkien"
+
+=====
+Exercise 2
+=====
+Now use an if/else statement to change the output depending on whether you read it yet or not. 
+
+If you read it, log a string like 'You already read "The Hobbit" by J.R.R. Tolkien', and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
+
+**/
+
+let books = [];
