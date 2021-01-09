c9e39876bafa6e9422302f57428c17edec758e70
Mark Farmiloe <mark@ccsdev.net>
2021-01-09 13:15:15 +0000

    Small adjustments to the text in the exercises. And one variable typo correction.

--- mandatory/6-reading-list.js
+++ mandatory/6-reading-list.js
@@ -1,7 +1,7 @@
 /**
 
 The Reading List
-Keep track of which books you read and which books you want to read!
+Keep track of which books you've read and which books you want to read!
 
 =====
 Exercise 1
@@ -11,14 +11,15 @@ Loop through the array of books. For each book, log the book title and book auth
 
 "The Hobbit by J.R.R. Tolkien"
 
-You should write and log at least 5 recipes
+You should write and log at least 5 books
 
 =====
 Exercise 2
 =====
-Now use an if/else statement to change the output depending on whether you read it yet or not. 
+Now use an if/else statement to change the output depending on whether you have read it yet or not. 
 
-If you read it, log a string like 'You already read "The Hobbit" by J.R.R. Tolkien', and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
+If you've read it, log a string like 'You've already read "The Hobbit" by J.R.R. Tolkien', 
+and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
 
 **/
 
