8b4485f8f4a97ae9032122ab44518d3f9e16e261
SenaitEkubai <73396195+SenaitEkubai@users.noreply.github.com>
2021-01-12 23:30:49 +0000

    Answered recipe exercise

--- mandatory/5-recipes.js
+++ mandatory/5-recipes.js
@@ -25,10 +25,51 @@ You should write and log at least 5 recipes
 **/
 
 let recipes = [
-  { title: "Porridge", servings: 2, ingredients: ["oats", "milk"] },
+  {
+    title: "Porridge",
+    servings: 2,
+    ingredients: ["oats", "milk"],
+  },
   {
     title: "Omelet",
     servings: 3,
     ingredients: ["eggs", "oil", "onions", "tomato"],
   },
+  {
+    title: "Beans",
+    servings: 2,
+    ingredients: ["canned beans", "Bread"],
+  },
+  {
+    title: "Rice",
+    servings: 2,
+    ingredients: ["rice", "water", "mixed vegetable", "olive oil"],
+  },
+  {
+    title: "Lentil Soup",
+    servings: 2,
+    ingredients: [
+      "lentils",
+      "water",
+      "mixed vegetable",
+      "oil",
+      "garlic",
+      "chili",
+    ],
+  },
+  {
+    title: "Pizza",
+    servings: 2,
+    ingredients: ["pizza dough", "salami", "cheese", "toppings"],
+  },
 ];
+
+recipes.forEach((element) => {
+  console.log(element.title);
+  console.log(`serves: ${element.servings}`);
+
+  console.log("Ingredients:");
+  for (var i = 0; i < element.ingredients.length; i++) {
+    console.log(`${element.ingredients[i]}`);
+  }
+});
