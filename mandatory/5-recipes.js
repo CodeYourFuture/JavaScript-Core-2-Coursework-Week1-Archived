a68be7cb4ccb460efc1c3d5090630e0342eaf426
mame-baba <mamebabambaye@gmail.com>
2021-01-15 23:38:08 +0000

    update-5 recipes

--- mandatory/5-recipes.js
+++ mandatory/5-recipes.js
@@ -1,14 +1,14 @@
-/** 
+/**
 
 The Recipe Card
 Never forget another recipe!
 
-Create an object to hold information on your favorite recipe. 
+Create an object to hold information on your favorite recipe.
 
-It should have properties for 
+It should have properties for
 
-- Title (a string), 
-- Servings (a number), and 
+- Title (a string),
+- Servings (a number), and
 - Ingredients (an array of strings)
 
 On separate lines (one console.log statement for each), log the recipe information so it looks like:
@@ -24,10 +24,14 @@ You should write and log at least 5 recipes
 
 **/
 
-let recipes = 
+let recipes =
 [ {
     title: "Jolof",
     serves: 3,
-    Ingredients:[] 
+    Ingredients:["onions", "peppers", "leaks"]
 
-} ]
+} ];
+// console.log(recipes[0].title);
+// console.log(recipes[1].serves);
+// recipes.forEach();
+console.log(Object.values(recipes));
