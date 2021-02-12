cf5cf3d7bc20fecad9983de7df1093ea18e41060
Mitch Lloyd <mitch@northcoders.com>
2021-02-12 09:27:54 +0000

    bring in line with default prettiter settings

--- mandatory/3-groceries.js
+++ mandatory/3-groceries.js
@@ -13,12 +13,12 @@ Complete the exercises below.
 
 // Here is your
 let weeklyMealPlan = {
-  monday: ['Cheese', 'Eggs', 'Tomato', 'Paprika', 'Leek'],
-  tuesday: ['Wrap', 'Tuna', 'Canned beans', 'Cheese', 'Carrot', 'Aubergine'],
-  wednesday: ['Orange Juice', 'Apple', 'Ananas', 'Black tea'],
-  thursday: ['Lamb', 'Salt', 'Bulgur', 'Potato'],
-  friday: ['Rice milk', 'Blueberries', 'Porridge', 'Banana', 'Cinnamon'],
-  saturday: ['Olive oil', 'Potato', 'Salmon', 'Asparagus'],
+  monday: ["Cheese", "Eggs", "Tomato", "Paprika", "Leek"],
+  tuesday: ["Wrap", "Tuna", "Canned beans", "Cheese", "Carrot", "Aubergine"],
+  wednesday: ["Orange Juice", "Apple", "Ananas", "Black tea"],
+  thursday: ["Lamb", "Salt", "Bulgur", "Potato"],
+  friday: ["Rice milk", "Blueberries", "Porridge", "Banana", "Cinnamon"],
+  saturday: ["Olive oil", "Potato", "Salmon", "Asparagus"],
   sunday: [],
 };
 
