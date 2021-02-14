2b0e4d8da068a3a5584bbed7b8dabf24b7d620b5
Murat Demirtas <73036794+muratdemirtas2001@users.noreply.github.com>
2021-02-14 22:27:06 +0000

    completing manddatory/2-water-bottle.js

--- mandatory/2-water-bottle.js
+++ mandatory/2-water-bottle.js
@@ -24,18 +24,29 @@ let bottle = {
   volume: 0,
   fillUp: function () {
     // calling this function should completely fill your bottle (volume = 100);
+    this.volume = 100;
   },
   pour: function () {
     // calling this function should increase your bottle volume by 10 units;
+    this.volume += 10;
+    if (this.volume > 100) {
+      this.volume = 100;
+    }
   },
   drink: function () {
     // calling this function should decrease your bottle volume by 10 units;
+    this.volume -= 10;
+    if (this.volume < 0) {
+      this.volume = 0;
+    }
   },
   isFull: function () {
     // this function should return true if your bottle is full;
+    return this.volume === 100;
   },
   isEmpty: function () {
     // this function should return true if your bottle is empty;
+    return this.volume === 0;
   },
 };
 
@@ -52,7 +63,7 @@ Extra question:
 */
 
 // Write you answer to the question here
-
+// I think it is easy to understand using this as this is referencing the Object.Sso we don't mind the name of the object.
 /*
 Once you have completed your object run the following 
 and see if your answer matches the expected result at the bottom :)
