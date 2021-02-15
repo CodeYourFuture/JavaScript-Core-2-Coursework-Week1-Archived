f1d3deb748bc7bd4aacba18ae2cdd21a7e228e1f
Murat Demirtas <73036794+muratdemirtas2001@users.noreply.github.com>
2021-02-15 12:33:22 +0000

    completing mandatory/7-budget.js

--- mandatory/7-budgets.js
+++ mandatory/7-budgets.js
@@ -16,7 +16,9 @@ Should give return the answer of 62600.
 
 **/
 
-function getBudgets(peopleArray) {}
+function getBudgets(peopleArray) {
+  return peopleArray.map((person) => person.budget).reduce((a, b) => a + b, 0);
+}
 
 /*
 ==================================================
