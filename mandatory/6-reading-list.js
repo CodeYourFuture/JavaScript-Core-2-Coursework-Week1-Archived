d294c675f2aa6ae12928000274a3b637ebe54d4d
Murat Demirtas <muratdemirtas2001@gmail.com>
2021-02-17 23:18:08 +0000

    completing mandatory/6-reading-list.js

--- mandatory/6-reading-list.js
+++ mandatory/6-reading-list.js
@@ -22,5 +22,23 @@ If you've read it, log a string like 'You've already read "The Hobbit" by J.R.R.
 and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
 
 **/
+//Exercise 1
+let books = [
+  { name: "War and Peace", author: "Tolstoy", didRead: true },
+  { name: "Song of Solomon", author: "Morrison", didRead: false },
+  { name: "Shadow of the wind", author: "Zafon", didRead: false },
+  { name: "The lord of the rings", author: "Tolkien", didRead: true },
+  { name: "Don Quixote ", author: "Cervantes", didRead: true },
+];
+books.forEach((book) => {
+  console.log(`${book.name} by ${book.author}`);
+});
 
-let books = [];
+//Exercise 2
+books.forEach((book)=>{
+if (book.didRead){
+    console.log(`You've already read "${book.name}" by ${book.author}`);
+} else {
+    console.log(`You still need to read "${book.name}" by ${book.author}`);
+}
+})
