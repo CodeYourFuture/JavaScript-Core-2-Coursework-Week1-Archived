8accdc033955fdc5c6562c779f2fde1c71ae6799
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-02-17 23:15:48 +0200

    exercise2

--- mandatory/9-class-exercises.js
+++ mandatory/9-class-exercises.js
@@ -1,6 +1,20 @@
-/*
-The objects below have some syntax issues - try and fix them all!
-*/
+/**
+ * exercise1
+ * Describe your own laptop as a JavaScript object. Try to think of as many properties as you can!
+ */
+let desktop = {
+    caseType: "Tower",
+    mouse: "Ifrared",
+    screenSize: 25,
+    brand: "Dell",
+    camera: false,
+    speakers: true,
+};
+
+/**
+ * exercise3
+ * The objects below have some syntax issues - try and fix them all!
+ */
 
 let kitten = {
     furColour: "orange",
