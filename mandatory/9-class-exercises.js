c2dbb8dc0eedabd442c0bb064d9238a473b23837
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-02-18 17:29:54 +0200

    exercise2

--- mandatory/9-class-exercises.js
+++ mandatory/9-class-exercises.js
@@ -13,17 +13,33 @@ let desktop = {
 
 /**
  * exercise2
- * Exercise (2)#
+ * Exercise (2)
  * Think of 5 different real world "things" that you can describe with a JavaScript object
  * Assign each of them to a separate variable
  */
+let transport = {
+    type: "bicycle",
+    distance: "20km",
+    timesPerWeek: 5,
+    reason: "It's what I can afford.",
+}
+console.log(`I own a ${transport.type} and I cover ${transport.distance}, ${transport.timesPerWeek} times per week because ${transport.reason}`);
 
+let person = {
+    name: "Douglas",
+    surname: "Van Der Merwe",
+    hairTypeAndColor: "curly, black",
+    eyes: "yes",
+    hasTwoArms: true,
+    hasTwoLegs: "yes",
+    hasPaws: false,
+}
+console.log(`Hi my name is ${person.name} and my surname is ${person.surname}. I have ${person.hairTypeAndColor} hair.`)
 
 /**
  * exercise3
  * The objects below have some syntax issues - try and fix them all!
  */
-
 let kitten = {
     furColour: "orange",
     age: "23",
