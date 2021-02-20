2d45d759af570c3bcf356637fcd9f8b03c36c59f
daniel <danielghanbari7@gmail.com>
2021-02-20 21:07:51 +0000

    7-budgets

--- mandatory/7-budgets.js
+++ mandatory/7-budgets.js
@@ -16,7 +16,13 @@ Should give return the answer of 62600.
 
 **/
 
-function getBudgets(peopleArray) {}
+function getBudgets(peopleArray) {
+  let total = 0;
+  for (let ele of peopleArray) {
+    total += ele.budget;
+  }
+  return total;
+}
 
 /*
 ==================================================
