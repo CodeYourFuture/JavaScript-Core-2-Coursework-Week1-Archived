6c28336ae94501007fb284b10dd1365dddc90ec1
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-02-20 21:20:38 +0200

    exercise3

--- mandatory/9-class-exercises.js
+++ mandatory/9-class-exercises.js
@@ -65,3 +65,7 @@ let phone = {
     megapixels: 12,
     batteryLife: "24 hours",
 };
+console.log(`The ${kitten.furColour} kitten is ${kitten.age} years old.`)
+console.log(`The ${kitten.age} years old, ${kitten.furColour} kitten owns a ${laptop.brand} laptop with ${laptop.ram} of ram.`)
+console.log(`The ${kitten.age} years old, ${kitten.furColour} kitten owns an ${phone.operatingSystem} phone with ${phone.batteryLife}
+ of battery life and a ${phone.megapixels} megapixel front camera.`)
