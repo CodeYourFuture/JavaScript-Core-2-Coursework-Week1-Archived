938643753097101f785f5e3665f0eea1b2345f26
Kara Howard <kara.mh191@gmail.com>
2021-02-21 14:13:52 +0000

    Mandatory 5 - Recipes complete

--- mandatory/5-recipes.js
+++ mandatory/5-recipes.js
@@ -24,4 +24,45 @@ You should write and log at least 5 recipes
 
 **/
 
-let recipes = {};
+let recipes = [
+    {
+        Title: "Chorizo & mozzarella gnocchi bake",
+        Servings: 6,
+        Ingredients: ["olive oil", "onion", "garlic", 
+                    "chorizo", "tomatoes", "caster sugar",
+                    "fresh gnocchi", "mozzarella", "basil"]
+    },
+    {
+        Title: "Chicken fajitas",
+        Servings: 3,
+        Ingredients: ["chicken", "onion", "red pepper", 
+                    "red chilli", "paprika", "coriander",
+                    "tortillas", "garlic", "olive oil"]
+    },
+    {
+        Title: "Piri-piri chicken with smashed sweet potatoes & broccoli",
+        Servings: 3,
+        Ingredients: ["chicken", "sweet potatoes", "oil", 
+                    "red onion", "piri-piri spice mix", "long-stem broccoli"]
+    },
+    {
+        Title: "Carrot cake",
+        Servings: 10,
+        Ingredients: ["natural yogurt", "eggs", "orange zested", 
+                    "self-raising flour", "light muscovado sugar", "cinnamon",
+                    "nutmeg"]
+    },
+    {
+        Title: "Treacle sponge",
+        Servings: 8,
+        Ingredients: ["golden syrup", "lemon zest", "breadcrumb", 
+                    "butter", "golden caster sugar", "eggs",
+                    "self-raising flour", "milk"]
+    }
+];
+
+for (recipe of recipes){
+    console.log(recipe.Title);
+    console.log(`Serves: ${recipe.Servings}`);
+    console.log(`Ingredients: ${recipe.Ingredients}\n`);
+}
