de54e0f5bedd920f58d5b0c3271e5d14e48b573f
Kudah Shambare <Kudahshambare263@gmail.com>
2021-02-23 05:47:34 +0200

    Water Bottle Done

--- mandatory/2-water-bottle.js
+++ mandatory/2-water-bottle.js
@@ -23,18 +23,28 @@ You have to implement the missing features according to the specification.
 let bottle = {
   volume: 0,
   fillUp: function () {
+    this.volume = 100;
     // calling this function should completely fill your bottle (volume = 100);
   },
   pour: function () {
+    if (this.volume < 91) {
+      this.volume += 10;
+    }
     // calling this function should increase your bottle volume by 10 units;
   },
   drink: function () {
+    if (this.volume >= 10) this.volume -= 10;
+
     // calling this function should decrease your bottle volume by 10 units;
   },
   isFull: function () {
+    if (this.volume === 100) return true;
+    else return false;
     // this function should return true if your bottle is full;
   },
   isEmpty: function () {
+    if (this.volume === 0) return true;
+    else return false;
     // this function should return true if your bottle is empty;
   },
 };
