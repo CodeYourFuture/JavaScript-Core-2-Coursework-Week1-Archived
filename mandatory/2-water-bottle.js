745ccc1de19f9e9483cb433c39bf67a07564c28d
Simba Pfaira <spfaira@yahoo.com>
2021-02-25 23:06:14 +0200

    Finished exercise 2 - water bottle

--- mandatory/2-water-bottle.js
+++ mandatory/2-water-bottle.js
@@ -24,18 +24,23 @@ let bottle = {
   volume: 0,
   fillUp: function () {
     // calling this function should completely fill your bottle (volume = 100);
+    this.volume = 100;
   },
   pour: function () {
     // calling this function should increase your bottle volume by 10 units;
+    if (this.volume < 100) this.volume += 10;
   },
   drink: function () {
     // calling this function should decrease your bottle volume by 10 units;
+    if (this.volume > 0) this.volume -= 10;
   },
   isFull: function () {
     // this function should return true if your bottle is full;
+    return this.volume == 100;
   },
   isEmpty: function () {
     // this function should return true if your bottle is empty;
+    return this.volume == 0;
   },
 };
 
@@ -52,6 +57,7 @@ Extra question:
 */
 
 // Write you answer to the question here
+//If you change your variable name you have to change all the places you have referred the object
 
 /*
 Once you have completed your object run the following 
