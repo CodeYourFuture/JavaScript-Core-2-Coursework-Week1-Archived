f8d4222b0299a19094f24a8b83fdce4675597f37
Simba Pfaira <spfaira@yahoo.com>
2021-02-26 02:07:32 +0200

    Completed exercise 7 - budgets

--- mandatory/7-budgets.js
+++ mandatory/7-budgets.js
@@ -16,7 +16,11 @@ Should give return the answer of 62600.
 
 **/
 
-function getBudgets(peopleArray) {}
+function getBudgets(peopleArray) {
+  budgetsTotal = 0;
+  peopleArray.forEach(person => budgetsTotal += person.budget);
+  return budgetsTotal;
+}
 
 /*
 ==================================================
