1821b3c6f19aa7377059dce06def444dab946b88
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-02-26 11:47:56 +0200

    Complete 2-water-bottle

--- mandatory/2-water-bottle.js
+++ mandatory/2-water-bottle.js
@@ -26,10 +26,18 @@ let bottle = {
     this.volume = 100;// calling this function should completely fill your bottle (volume = 100);
   },
   pour: function () {
-    this.volume = this.volume + 10;// calling this function should increase your bottle volume by 10 units;
+    if (this.volume <100){
+    this.volume = this.volume + 10;
+    } else if(this.volume === 100){
+      return "Bottle full";
+    }// calling this function should increase your bottle volume by 10 units;
   },
   drink: function () {
-   this.volume = this.volume - 10; // calling this function should decrease your bottle volume by 10 units;
+    if(this.volume >=10){
+   this.volume = this.volume - 10;
+     }else if(this.volume === 0){
+       return "Sorry! Bottle is empty, please re-fill";
+     } // calling this function should, decrease your bottle volume by 10 units;
   },
   isFull: function () {
     if (this.volume === 100){
@@ -165,7 +173,7 @@ if (bottle.isEmpty()) {
 }
 
 if (bottle.volume === 0) {
-  console.log(`That's correct! Empty bottle volume is repesented as zero.`);
+  console.log(`That's correct! Empty bottle volume is represented as zero.`);
 } else {
   failed = true;
 
