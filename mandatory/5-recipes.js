a4d2bf7b8f591dc4c34af1ef0b5c4e4344518499
LawrenceBaatjies <lawrencebaatjies47@gmail.com>
2021-02-27 00:36:38 +0200

    5-recipes.js complete
    Exercise 5  completed

--- mandatory/5-recipes.js
+++ mandatory/5-recipes.js
@@ -24,4 +24,43 @@ You should write and log at least 5 recipes
 
 **/
 
-let recipes = {};
+let recipes = [
+    
+    {
+
+    title: "Pancakes",
+    serves: 2,
+    ingredients: ["water","flower","two eggs","surger","milk","cinnamon",]
+
+ },
+ {
+    title: "French Omelette",
+    serves: 1,
+    ingredients: ["6 eggs ", "avocado", "fried bacon","chives","Spinach","tomato","feta","Parmesan"],
+  },
+  {
+    title: "Butternut Soup",
+    serves: 6,
+    ingredients: ["brown onions","butternut", "pumpkin", "full cream milk", "spices"],
+  },
+  {
+    title: "Braai Pie",
+    serves: 8,
+    ingredients: ["two rolls of puff pastry ", "sautéed sliced mushrooms", "cheese", "cooked onion", "pepper seasoning","pne whisked egg"],
+  },
+  {
+    title: "Stuffed Ham & Egg Bread",
+    serves: 4,
+    ingredients: ["diced tomatoes", "6 large eggs", "cooked ham", "crusty French loaf","cheddar cheese"],
+
+  },
+];
+
+
+for (let elem of recipes) {
+    console.log(elem.title);
+    console.log("serves:", elem.serves);
+    console.log("ingredients:");
+    elem.ingredients.map((x) => console.log(x));
+    console.log("");
+  }
