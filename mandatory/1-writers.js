b7609b3b8385de27f7ef17061a763b184704c142
Khaya56 <36347158+Khaya56@users.noreply.github.com>
2021-03-01 15:54:54 +0200

    First exercises

--- mandatory/1-writers.js
+++ mandatory/1-writers.js
@@ -59,7 +59,10 @@ Exercise 1:
 
   "Hi, my name is {firstName} {lastName}. I am {age} years old, and work as a {occupation}."
 */
+for (let i = 0; i < writers.length; i++) {
 
+  console.log(`Hi,my name is ${writers[i].firstName} ${writers[i].lastName}. I am ${writers[i].age} years old, and work as a ${writers[i].occupation}.`);
+}
 /*
 Exercise 2:
 
@@ -68,6 +71,11 @@ Exercise 2:
 
   "Writer {firstName} {lastName} died at {age} years old."
 */
+for (let i = 0; i < writers.length; i++) {
+  if (writers[i].age >= 40 && writers[i] <= 49 && writers[i].alive === false) {
+    console.log(`Writer ${writers[i].firstName} ${writers[i].lastName} died at ${writers[i].age} years old.`);
+  }
+}
 
 /*
 Exercise 3:
@@ -76,3 +84,8 @@ Exercise 3:
 
   "Hi, my name is {firstName} {lastName}. I am {age} years old."
 */
+for (let i = 0; i < writers.length; i++) {
+  if (writers[i].age >= 40 && writers[i] <= 49 && writers[i].alive === true) {
+    console.log(`Hi, my name is ${writers[i].firstName} ${writers[i].lastName}. I am ${writers[i].age} years old.`);
+  }
+}
