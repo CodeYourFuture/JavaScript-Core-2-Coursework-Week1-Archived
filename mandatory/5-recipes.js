8eecb7c39b70341525d010b992b0c12bf86d3609
soraiya <soraiyamm@gmail.com>
2021-03-10 12:49:13 +0000

    exercise 5

--- mandatory/5-recipes.js
+++ mandatory/5-recipes.js
@@ -32,9 +32,55 @@ let recipes = {
 
 console.log(recipes.title);
 console.log("Serves:", recipes.servings);
+console.log("Ingredients:");
 for (let i of recipes.ingredients) {
-    if (i === 0) {
-    console.log("Ingredients:");
-  }
+  console.log(i.toString());
+}
+
+let recipe2 = {
+  title: "Coconut, Lentil Curry",
+  servings: 3,
+  ingredients: ["red lentils", "sweet potatoes", "coconut milk", "curry powder", "spinach"]
+}
+console.log(recipe2.title);
+console.log("Serves:", recipe2.servings);
+console.log("Ingredients:");
+for (let i of recipe2.ingredients) {
+  console.log(i.toString());
+}
+
+let recipe3 = {
+  title: "Honey Baked Salmon",
+  servings: 2,
+  ingredients: ["salmon", "honey", "butter", "garlic", "seasoning"]
+}
+console.log(recipe3.title);
+console.log("Serves:", recipe3.servings);
+console.log("Ingredients:");
+for (let i of recipe3.ingredients) {
+  console.log(i.toString());
+}
+
+let recipe4 = {
+  title: "Ratatouille",
+  servings: 2,
+  ingredients: ["aubergine", "tomatoes", "courgette", "garlic", "mixed herbs"]
+}
+console.log(recipe4.title);
+console.log("Serves:", recipe4.servings);
+console.log("Ingredients:");
+for (let i of recipe4.ingredients) {
+  console.log(i.toString());
+}
+
+let recipe5 = {
+  title: "Roast Vegetables",
+  servings: 4,
+  ingredients: ["carrots", "parsnips", "potatoes", "bell peppers", "olive oil"]
+}
+console.log(recipe5.title);
+console.log("Serves:", recipe5.servings);
+console.log("Ingredients:");
+for (let i of recipe5.ingredients) {
   console.log(i.toString());
 }
