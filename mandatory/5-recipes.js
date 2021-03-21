0cffc88d6c17f8c2faf70639ba74c5e14fabec11
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-03-21 12:44:36 +0200

    Complete 5-recipes

--- mandatory/5-recipes.js
+++ mandatory/5-recipes.js
@@ -3,7 +3,7 @@
 The Recipe Card
 Never forget another recipe!
 
-Create an object to hold information on your favorite recipe. 
+Create an object to hold information on your favorite recipes. 
 
 It should have properties for 
 
@@ -26,15 +26,43 @@ You should write and log at least 5 recipes
 
 let recipes = [
     {
-        Title: "scrambled eggs",
+        Title: "Scrambled eggs",
     Servings: 2,
     Ingredients: ["eggs", "bread", "butter"]
 },
     {
-    Title: "boeber",
+    Title: "Boeber",
     Servings: 4, 
-    Ingredients: ["Vermicelli", "Milk", "Cinnamon", "Sugar"]},  
+    Ingredients: ["Vermicelli", "Milk", "Cinnamon", "Sugar","Sago"]
+},
+ {
+    Title: "Koesisters",
+    Servings: 6, 
+    Ingredients: ["Cake Flour", "Milk", "Aniseed", "Sugar", "Baking powder","Water"]
+},
+{
+    Title: "Spaghetti bolognese",
+    Servings: 4, 
+    Ingredients: ["Spaghetti", "Mince", "Tomato Paste", "Sugar", "Cinnamon Sticks","Black Pepper","Salt","Tomatoes","Basil","Bay leaves","Pimento"]
+},
+{
+    Title: "Chicken curry masala",
+    Servings: 4, 
+    Ingredients: ["Skinless chicken breasts", "Onion", "Salt", "Curry powder","Bay leaves","Tumeric powder","Masala powder","Basmati rice","Tomato salad"]
+},
+
 ];
-for (let recipe of recipes){
+
+let recipeCard = function (recipeArr){
+for (let recipe of recipeArr){
 console.log(recipe.Title);
+console.log("Serves: ", recipe.Servings);
+console.log("Ingredients: ");
+recipe.Ingredients.forEach(element => {
+    console.log(element);
+});
+console.log(" ");
 }
+};
+
+recipeCard(recipes);
