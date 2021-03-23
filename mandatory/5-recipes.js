77110dd450d009c4c322f3b9ecf6b368899a46d6
Aashiq Abrahams <aashiqabrahams44@gmail.com>
2021-03-23 21:34:08 +0200

    5-recipes-completed

--- mandatory/5-recipes.js
+++ mandatory/5-recipes.js
@@ -24,4 +24,38 @@ You should write and log at least 5 recipes
 
 **/
 
-let recipes = {};
+let recipes = [
+    {
+        Title: "Black tahini noodles",
+        Servings: 2,
+        Ingredients: ["fine rice noodle","limes","punnet of cress","black sesame seeds","teriyaki sauce"]
+    },
+    {
+        Title: "Liver, macon & onions",
+        Servings: 3,
+        Ingredients: ["red onion","sage","sourdough bread","macon","liver"]
+    },
+    {
+        Title: "Lentil salad",
+        Servings: 4,
+        Ingredients: ["anchovies","lemon","broccoli","chillies","lentils"]
+    },
+    {
+        Title: "Lamb stew",
+        Servings: 5,
+        Ingredients: ["rosemary","lamb","olives","onions","tomatoes"]
+    },
+    {
+        Title: "Tuna pasta",
+        Servings: 6,
+        Ingredients: ["pasta shells","capers","tomatoes","black oregano","tuna"]
+    }
+];
+
+for (recipe of recipes){
+    console.log(recipe.Title);
+    console.log('Serves: ${recipe.Servings}');
+    console.log('Ingredients: ${recipe.Ingredients}\n');
+}
+    
+
