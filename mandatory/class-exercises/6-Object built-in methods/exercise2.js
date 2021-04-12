4b422c159208b54458f526ebb67a427e54ebf629
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-04-12 23:16:50 +0200

    Update exercise2.js

--- mandatory/class-exercises/6-Object built-in methods/exercise2.js
+++ mandatory/class-exercises/6-Object built-in methods/exercise2.js
@@ -15,9 +15,11 @@ let mentorsAges = {
 
 // ONLY EDIT BELOW THIS LINE
 
-let mentorsNames = ;
+let mentorsNames = Object.keys(mentorsAges);
 
-let mentorsNamedUppercased = ;
+let mentorsNamedUppercased = mentorsNames.map(mentorsName =>
+  mentorsName.toUpperCase()
+);
 
 // ONLY EDIT ABOVE THIS LINE
 
