47588caa2ce1f3ad292fdec57a3eac87ee007a63
Khaya56 <khayab14@gmail.com>
2021-04-18 16:30:09 +0200

    Update writers

--- mandatory/1-writers.js
+++ mandatory/1-writers.js
@@ -1,21 +1,21 @@
-/* 
+/*
   Challenge 1: Famous Writers
 
-  Did you know you can also have an Array of Objects? 
+  Did you know you can also have an Array of Objects?
 
   You might think "This is madness!" but in everyday coding life
   it is quite a frequent combination. Just think about what benefits we can get from this construct.
 
   An object lets you store multiple values in a single variable, then you can store complex objects in an array.
   Let's assume you have a list of data about people names and their birthday and you would like to print each name
-  with corresponding birthday together. 
+  with corresponding birthday together.
 
   Storing these pieces of information in different arrays and then pairing them up
   makes the iteration unnecessarily complicated, code will be less intuitive, needs extra cognitive effort to
   reason about and last but not least it can be error-prone (for example, you match up the wrong birthday to a name).
 
-  In this exercise you will practice how to access Objects stored in an Array and their properties. You already know 
-  different ways of looping through Arrays, it won't be different in this case. The only extra step is that you have to 
+  In this exercise you will practice how to access Objects stored in an Array and their properties. You already know
+  different ways of looping through Arrays, it won't be different in this case. The only extra step is that you have to
   use values inside Objects.
 */
 
@@ -59,7 +59,9 @@ Exercise 1:
 
   "Hi, my name is {firstName} {lastName}. I am {age} years old, and work as a {occupation}."
 */
-
+writers.forEach((writer)=> {
+  console.log(`Hi, my name is ${writer.firstName} ${writer.lastName}. I am ${writer.age} years old, and work as a ${writer.occupation}.`);
+});
 /*
 Exercise 2:
 
@@ -68,6 +70,11 @@ Exercise 2:
 
   "Writer {firstName} {lastName} died at {age} years old."
 */
+writers.forEach((writer) =>{
+  if (writer.occupation === "writer" && writer.age >= 40 && writer.age <= 49 && !writer.alive) {
+    console.log(`Writer ${writer.firstName} ${writer.lastName} died at ${writer.age} years old`);
+  }
+});
 
 /*
 Exercise 3:
@@ -76,3 +83,8 @@ Exercise 3:
 
   "Hi, my name is {firstName} {lastName}. I am {age} years old."
 */
+writers.forEach((writer)=>{
+  if (writer.occupation === "writer" && writer.age >= 40 && writer.age <= 49 && writer.alive) {
+    console.log(`Hi, my name is ${writer.firstName} ${writer.lastName}. I am ${writer.age} years old`);
+  }
+});
