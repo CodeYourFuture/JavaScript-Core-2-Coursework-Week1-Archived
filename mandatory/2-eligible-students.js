753b2c069e59b57b1fb1c5576b50ef18c9a4453a
Texx45 <tcalderbank@hotmail.com>
2021-07-19 14:02:27 +0100

    eligible-students completed

--- mandatory/2-eligible-students.js
+++ mandatory/2-eligible-students.js
@@ -19,8 +19,15 @@
   - Returns an array containing only the names of the who have attended AT LEAST 8 classes
  */
 
+//! *** Steve I couldn't work out how to filter the names out individually without using an empty array and pushing them to it ***
+
 function eligibleStudents(attendances) {
-   
+  finalStudents = [];
+  const eightOrMore = attendances.filter((student) => student.attendance >= 8);
+  for (const student of eightOrMore) {
+    finalStudents.push(student.name);
+  }
+  return finalStudents;
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== 
@@ -30,15 +37,19 @@ function eligibleStudents(attendances) {
 */
 
 const attendances = [
-    {name: "Ahmed", attendance: 8},
-    {name: "Clement", attendance: 10},
-    {name: "Elamin", attendance: 6},
-    {name: "Adam", attendance: 7},
-    {name: "Tayoa", attendance: 11},
-    {name: "Nina", attendance: 10},
+  { name: "Ahmed", attendance: 8 },
+  { name: "Clement", attendance: 10 },
+  { name: "Elamin", attendance: 6 },
+  { name: "Adam", attendance: 7 },
+  { name: "Tayoa", attendance: 11 },
+  { name: "Nina", attendance: 10 },
 ];
-  
+
 test("eligibleStudents function works", () => {
-    expect(eligibleStudents(attendances)).toEqual(["Ahmed", "Clement", "Tayoa", "Nina"]);
+  expect(eligibleStudents(attendances)).toEqual([
+    "Ahmed",
+    "Clement",
+    "Tayoa",
+    "Nina",
+  ]);
 });
- 
