c35329408b201c6245bff2081805bf58f43fb8bc
omerffaruk <farukyamak11044@gmail.com>
2021-07-19 16:47:41 +0100

    Mandatory 4-water-bottle.js exercise done

--- mandatory/4-water-bottle.js
+++ mandatory/4-water-bottle.js
@@ -24,18 +24,26 @@ let bottle = {
   volume: 0,
   fillUp: function () {
     // calling this function should completely fill your bottle (volume = 100);
+    this.volume = 100;
   },
   pour: function () {
     // calling this function should increase your bottle volume by 10 units;
+    if(this.volume >= 0 && this.volume <= 90) this.volume += 10;
+    else if(this.volume > 90 && this.volume < 100) this.volume += (100 - this.volume);
+    else if(this.volume === 100) this.volume = 100;
   },
   drink: function () {
     // calling this function should decrease your bottle volume by 10 units;
+    if(this.volume >= 10) this.volume -= 10;
+    else if(this.volume < 10) this.volume = 0;
   },
   isFull: function () {
     // this function should return true if your bottle is full;
+    return this.volume === 100;
   },
   isEmpty: function () {
     // this function should return true if your bottle is empty;
+    return this.volume === 0;
   },
 };
 
@@ -52,7 +60,7 @@ Extra question:
 */
 
 // Write you answer to the question here
-
+//   This keyword is a better practice in terms of simplicity, and also for debugging reasons. Using variable name could cause confusion where there are many global variables, whereas this specifically states the object. 
 /*
 Once you have completed your object run the following 
 and see if your answer matches the expected result at the bottom :)
