edd9a7071aa863b0ad43f91108c0ecda94ea55bf
omerffaruk <farukyamak11044@gmail.com>
2021-07-20 23:02:06 +0100

    Mandatory 7-recipes.js done

--- mandatory/7-recipes.js
+++ mandatory/7-recipes.js
@@ -24,5 +24,39 @@ You should write and log at least 5 recipes
 
 **/
 
-let recipes = {};
+let recipes = {
+  recipe1: {
+    title: "Pancake",
+    serving: 5,
+    ingredients: ["milk", "flour", "egg"],
+  },
+  recipe2: {
+    title: "Pizza",
+    serving: 4,
+    ingredients: ["olive", "flour", "egg", "olive oil", "cheese"],
+  },
+  recipe3: {
+    title: "Fish & Chips",
+    serving: 4,
+    ingredients: ["salmon", "potato", "salt"],
+  },
+  recipe4: {
+    title: "Cheese Sandwich",
+    serving: 5,
+    ingredients: ["bread", "cheese", "salt"],
+  },
+  recipe5: {
+    title: "Baklava",
+    serving: 8,
+    ingredients: ["sugar", "flour", "egg", "hazelnut"],
+  },
+};
+
+
+for (const key in recipes) {
+    console.log(`${recipes[key].title}`);
+    console.log(`Serves: ${recipes[key].serving}`);
+    console.log(`Ingredients: `)
+    recipes[key].ingredients.forEach((el) => console.log(el));
+}
 
