b225159f3601fc2600586563adb9adc49269921f
Dharma Guadeloupe <dharmaguadeloupe@gmail.com>
2021-07-21 10:47:34 +0100

    2-eliglible-students

--- mandatory/2-eligible-students.js
+++ mandatory/2-eligible-students.js
@@ -20,8 +20,9 @@
  */
 
 function eligibleStudents(attendances) {
-   
-}
+   const eligibleStudentsAttendance = attendances.filter(student => student.attendance >= 8);
+  return eligibleStudentsAttendance.map(student => student.name);
+};
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 - To run the tests for this exercise, run `npm test -- --testPathPattern 2-eligible-students.js`
