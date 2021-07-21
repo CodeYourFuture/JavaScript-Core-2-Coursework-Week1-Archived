7a5e9126a31090e69d0346e239e8d5df8aef1e55
Dharma Guadeloupe <dharmaguadeloupe@gmail.com>
2021-07-21 11:45:29 +0100

    6-people-I-know exercise 1

--- mandatory/6-people-I-know.js
+++ mandatory/6-people-I-know.js
@@ -382,7 +382,7 @@ First, I want you to find all of my friends who are 35 or older.
 
 */
 
-let thirtyFiveOrOlder = [];
+let thirtyFiveOrOlder = friends.filter(friend => friend.age >= 35);
 
 /*
 3) Find the email address
