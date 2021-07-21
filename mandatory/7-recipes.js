55ee4bffb2d6b11d553c328e40c3732c5fecde5c
Dharma Guadeloupe <dharmaguadeloupe@gmail.com>
2021-07-21 12:59:18 +0100

    7-recipes.js

--- mandatory/7-recipes.js
+++ mandatory/7-recipes.js
@@ -24,5 +24,38 @@ You should write and log at least 5 recipes
 
 **/
 
-let recipes = {};
+let recipes = {
+  pizza: {
+    title: "Pepproni Pizza",
+    serves: 4,
+    size: "18 in",
+    ingredients: ["pizza-base", "tomato puree", "pepperoni", "cheese", "bacon", "mushrooms", "onions"]
+  },
+  sandwich: {
+    title: "Cheese & Jam Sandwich",
+    serves: 1,
+    ingredients: ["2 slices of bread", "butter", "cheese", "jam"]
+  },
+  roastChicken: {
+    title: "Roast Chicken",
+    serves: 4,
+    ingredients: ["whole chicken", "garlic", "butter", "olive oil", "lemon juice", "rosemary", "parsley"]
+  },
+  paella: {
+    title: "Spanish Paella",
+    serves: 4,
+    ingredients: ["prawns", "stock cube", "olive oil", "chorizo", "chicken breasts", "onion", "garlic", "rice", "peppers", "tomatoes", "peas", "lemon wedges"]
+  },
+  tunaPasta: {
+    title: "Tuna and Pasta Bake",
+    serves: 2,
+    ingredients: ["tuna", "pasta", "sweetcorn", "peppers", "seasoning"]
+  }
+};
 
+for(const recipe in recipes) {
+  console.log(`${recipes[recipe].title}`);
+  console.log(`Serves: ${recipes[recipe].serves}`);
+  console.log("Ingredients:");
+  recipes[recipe].ingredients.forEach(item => console.log(item));
+}
