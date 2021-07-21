f3b9fe132890335710d5c4eeccf46e3dd4b4ec1e
Maziarmajd <83129200+Maziarmajd@users.noreply.github.com>
2021-07-21 20:00:07 +0100

    Mandatory 7

--- mandatory/7-recipes.js
+++ mandatory/7-recipes.js
@@ -24,5 +24,42 @@ You should write and log at least 5 recipes
 
 **/
 
-let recipes = {};
+let recipes = {
+  steak : {
+    names:'Sirloin Steak',
+    service : 2,
+    ingredients :['Sirloin Beef', 'French fires', 'Garlic butter']
+  },
+  pasta : {
+    names:'Pasta Alfredo',
+    service : 3,
+    ingredients :['Penne', 'cream', 'chicken','mashroom','Parmigiano']
+  },
+  spagethi : {
+    names:'Spaghetti Carbonara',
+    service : 4,
+    ingredients :['spaghetti', 'bacon', 'pecorino cheese', 'eggs', 'parmesan', 'unsalted butter']
+  },
+  salmon : {
+    names:'Teriyaki salmon',
+    service : 2,
+    ingredients :['salmon fillet', 'dark soy sauce', 'egg noodles', 'ginger']
+  },
+  burger
+ : {
+    names:'Cheeseburger',
+    service : 1,
+    ingredients :['burger', 'french fries', 'Goda cheese', 'oil']
+  }
+};
+
+for(let recipe in recipes){
+  console.log(`${recipes[recipe].names}`);
+  console.log(`Serves: ${recipes[recipe].service}`);
+  console.log('Ingredients');
+  recipes[recipe].ingredients.forEach(element => {
+    console.log(element);
+  });
+  console.log('######## Maziar\'s recepies ################');
+}
 
