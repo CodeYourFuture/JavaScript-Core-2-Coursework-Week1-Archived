b0c7ba0837bbebd7b330c64250edb59c6174d8d1
Texx45 <tcalderbank@hotmail.com>
2021-07-22 13:46:35 +0100

    completed-homework-11-not-completed

--- mandatory/7-recipes.js
+++ mandatory/7-recipes.js
@@ -25,11 +25,56 @@ You should write and log at least 5 recipes
 **/
 
 let recipes = {
-  title: "Mole",
-  servings: 2,
-  ingredients: ["cinnamon", "cumin", "cocoa"],
+  spaghettiBolognese: {
+    title: "Spaghetti Bolognese",
+    serves: 4,
+    ingredients: [
+      "mince",
+      "tomato puree",
+      "peppers",
+      "garlic",
+      "mushrooms",
+      "onions",
+    ],
+  },
+  omelette: {
+    title: "Omelette",
+    serves: 1,
+    ingredients: ["eggs", "ham", "cheese", "mushroom"],
+  },
+  beansOnToast: {
+    title: "Beans On Toast",
+    serves: 1,
+    ingredients: ["bread", "butter", "beans"],
+  },
+  shepherdsPie: {
+    title: "shepherdsPie",
+    serves: 4,
+    ingredients: [
+      "onion",
+      "carrot",
+      "celery",
+      "carrot",
+      "beef stock",
+      "onion",
+      "tomato puree",
+      "peas",
+    ],
+  },
+  cheeseOnToast: {
+    title: "Cheese on Toast",
+    serves: 1,
+    ingredients: ["bread", "cheese", "worcestershire sauce"],
+  },
 };
 
-console.log(recipes.title);
-console.log(`Serves: ${recipes.servings}`);
-console.log(`ingredients:\n${recipes.ingredients.join("\n")}`);
+for (const recipe in recipes) {
+  console.log(`${recipes[recipe].title}`);
+  console.log(`Serves: ${recipes[recipe].serves}`);
+  console.log("Ingredients:");
+  recipes[recipe].ingredients.forEach((item) => console.log(item));
+}
+
+// console.log(recipes.title);
+// console.log(`Serves: ${recipes.servings}`);
+// console.log(`ingredients:\n${recipes.ingredients.join("\n")}`);
