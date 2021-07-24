9e1b8b6d8369d92ecded03ae26fa68c1ebba267b
Niamh_Madden <Niamhmadden15@gmail.com>
2021-07-24 11:37:08 +0100

    Added changes to water bottle exercise but can't pass test

--- mandatory/4-water-bottle.js
+++ mandatory/4-water-bottle.js
@@ -23,21 +23,32 @@ You have to implement the missing features according to the specification.
 let bottle = {
   volume: 0,
   fillUp: function () {
+    return this.volume += 100;
     // calling this function should completely fill your bottle (volume = 100);
   },
   pour: function () {
+    if (this.volume <= 90) {
+      return this.volume += 10;
     // calling this function should increase your bottle volume by 10 units;
-  },
+  }
+},
+
   drink: function () {
+    if(this.volume >= 10) {
+        return this.volume -=10;
+      }
+    
     // calling this function should decrease your bottle volume by 10 units;
   },
   isFull: function () {
+    return this.volume === 100;
     // this function should return true if your bottle is full;
   },
   isEmpty: function () {
+    return this.volume === 0
     // this function should return true if your bottle is empty;
-  },
-};
+  }
+  };
 
 /*
 TIP:
