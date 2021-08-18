1b82675aaa149decfb993ef5e45cccbd4df8cfb9
Benjamin-GitHub <benjaminmhrdd@gmail.com>
2021-08-18 14:25:07 +0100

    7-recipes
    Completed

--- mandatory/7-recipes.js
+++ mandatory/7-recipes.js
@@ -24,5 +24,76 @@ You should write and log at least 5 recipes
 
 **/
 
-let recipes = {};
+let recipes = {
+  Brownie: {
+    title: "Best Brownies",
+    servings: 16,
+    ingredients: [
+      "1/2 cup butter",
+      "1 cup white sugar",
+      "2 eggs",
+      "1 teaspoon vanilla",
+      "1/3 cup cocoa powder",
+      "1/2 cup flour",
+      "1/4 teaspoon salt",
+      "1/4 teaspoon baking powder",
+    ],
+  },
+  Cookie: {
+    title: "Whipped Cookies",
+    servings: 36,
+    ingredients: [
+      "1 cup butter",
+      "1 cup flour",
+      "1/2 cup sugar",
+      "1/4 cup green cherries",
+      "1/4 cup red cherries",
+    ],
+  },
+  Chicken: {
+    title: "Greek Chicken",
+    servings: 4,
+    ingredients: [
+      "1/4 cup lemon juice",
+      "1 teaspoon Greek seasoning",
+      "1 tablespoon garlic",
+      "1 lemon, zested",
+      "1 teaspoon ground black pepper",
+      "1/2 teaspoon dried thyme",
+      "3 skinless, boneless chicken breasts",
+    ],
+  },
+  BreakfastOne: {
+    title: "Quick Quiche",
+    servings: 6,
+    ingredients: [
+      "8 slices bacon",
+      "4 ounces shredded Swiss cheese",
+      "2 tablespoons butter, melted",
+      "4 eggs, beaten",
+      "1 teaspoon salt",
+      "1/4 cup flour",
+      "1/4 cup finely chopped onion",
+    ],
+  },
+  BreakfastTwo: {
+    title: "Breakfast Biscuits",
+    servings: 10,
+    ingredients: [
+      "1 biscuit dough",
+      "1 pound bacon",
+      "5 eggs",
+      "1/4 cup milk",
+      "3 tablespoons butter",
+      "10 slices Cheddar cheese",
+    ],
+  },
+};
 
+for (const recipe in recipes) {
+    console.log(`${recipe}: ${recipes[recipe].title}`);
+    console.log(`Serves: ${recipes[recipe].servings}`);
+    console.log(`Ingredient: `);
+    recipes[recipe].ingredients.forEach(elem => {console.log(elem)});
+    console.log("");
+}
