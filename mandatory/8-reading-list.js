0b88660c5a6af16ec34990dd7ab7ff4c41e4dfd8
Mor Khojasteh <mori.kh1987@gmail.com>
2021-08-23 00:03:47 +0100

    Add Reading List

--- mandatory/8-reading-list.js
+++ mandatory/8-reading-list.js
@@ -1,52 +1,72 @@
 /**
-
 The Reading List
 Keep track of which books you've read and which books you want to read!
-
 =====
 Exercise 1
 =====
-
 Create an array of objects, where each object describes a book and has properties for:
 - The title (a string)
 - Author (a string)
 - and alreadyRead (a boolean indicating if you read it yet)
-
 Write a funciton that loops through the array of books. For each book, log the book title and book author like so: 
-
 "The Hobbit by J.R.R. Tolkien"
-
 You should write and log at least 5 books. 
-
 You should modify the tests so that they contain the values that correspond to your books.
 In this style of testing it is typical to write out as strings exactly what you expect your output to be, 
 without using any variables or any logic like loops, template strings or if statements.
-
 */
+const books = [
+  {
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    alreadyRead: true
+  }, 
+  {
+    title: "The Map of Salt and Stars",
+    author: "Jennifer Zeynab Joukhadar",
+    alreadyRead: false
+  }, 
+  {
+    title: "Dietland",
+    author: "Sarai Walker",
+    alreadyRead: true
+  },
+  {  
+    title: "A Place for Us",
+    author: "Fatima Farheen Mirza",
+    alreadyRead: false
+  }, 
+  {
+    title: "The House of Impossible Beauties",
+    author: "Joseph Cassara",
+    alreadyRead: true
+  }
+];
 
-const books = [];
-  
 // exercise 1
 function logBooks() {
+  books.forEach((book) => {
+    if(book.alreadyRead){
+       console.log(`You've already read '${book.title}' by ${book.author}`);
+    } else {
+      console.log(`You still need to read '${book.title}' by ${book.author}`);
+    }
+  })
 }
-  
+
 
 /*
-
 =====
 Exercise 2
 =====
 Now modify the function, using an if/else statement to change the output depending on whether you have read it yet or not. 
-
 If you've read it, log a string like 'You've already read "The Hobbit" by J.R.R. Tolkien', 
 and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
-
 You will need to modify the tests to check the correct output. If you have already learnt about red-green refactoring,
 remember to practice:
 - first change the test to the value that should be output, 
 - run the test to check that your test goes red
 - now change your code to make the test pass
-
 As an example for this exercise, you might do the following steps
 - Modify the tests so that they all say 'You've already read <name> by <author>'
 - Run the test (they will all fail)
@@ -58,9 +78,7 @@ As an example for this exercise, you might do the following steps
 - Run the test (the test will fail but there will be some successful results)
 - Modify the books so that they have the correct alreadyRead value
 - All tests should turn green!!
-
 **/
-
 /* ======= TESTS - DO MODIFY (!!!) =====
 - To run the tests for this exercise, run `npm test -- --testPathPattern 8-reading-list.js`
 - To run all exercises/tests in the mandatory folder, run `npm test`
@@ -69,12 +87,12 @@ As an example for this exercise, you might do the following steps
 
 test("books are logged", function() {
  expectLogBooksToLog([
-        "The Hobbit by J.R.R. Tolkien",
-        "The Map of Salt and Stars by Jennifer Zeynab Joukhadar",
-        "Dietland by Sarai Walker",
-        "A Place for Us by Fatima Farheen Mirza",
-        "The House of Impossible Beauties by Joseph Cassara"
-    ]);
+    "You've already read 'The Hobbit' by J.R.R. Tolkien",
+    "You still need to read 'The Map of Salt and Stars' by Jennifer Zeynab Joukhadar",
+    "You've already read 'Dietland' by Sarai Walker",
+    "You still need to read 'A Place for Us' by Fatima Farheen Mirza",
+    "You've already read 'The House of Impossible Beauties' by Joseph Cassara",
+  ]);
 });
 
 /* ======= TESTS - DO NOT MODIFY ===== */
