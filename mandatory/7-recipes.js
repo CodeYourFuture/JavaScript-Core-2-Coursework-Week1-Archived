5d359542dd41ce9ed451f81beb2064bad9e4e4e6
Halla-S <halla.sulaiman.33@gmail.com>
2021-09-24 23:40:13 +0100

    mandatory 7

--- mandatory/7-recipes.js
+++ mandatory/7-recipes.js
@@ -24,5 +24,41 @@ You should write and log at least 5 recipes
 
 **/
 
-let recipes = {};
-
+let recipes = {
+  Mole: {
+    name: "Mole",
+    Serves: 2,
+    Ingredients: ["cinnamon", "cunim", "cocoa"],
+  },
+  Pie: {
+    name: "Pie",
+    Serves: 4,
+    Ingredients: ["cinnamon", "butter", "flour", "sugar"],
+  },
+  Cookies: {
+    name: "Cookies",
+    Serves: 4,
+    Ingredients: ["chocolate chips", "butter", "flour", "brown sugar"],
+  },
+  Pancake: {
+    name: "Pancake",
+    Serves: 2,
+    Ingredients: ["eggs", "milk", "flour", "sugar"],
+  },
+  Cupcake: {
+    name: "Cupcake",
+    Serves: 2,
+    Ingredients: ["eggs", "milk", "flour", "sugar","cocoa"],
+  },
+};
+for (recipe in recipes){
+console.log(recipe);
+let keys= Object.keys(recipes[recipe]);
+let values = Object.values(recipes[recipe]);
+console.log(keys[1] + " : " + values[1]);
+console.log(keys[2] +":");
+let ingredientsValues = values[2];
+ingredientsValues = ingredientsValues.forEach(element => {
+ console.log(element);
+});
+}
