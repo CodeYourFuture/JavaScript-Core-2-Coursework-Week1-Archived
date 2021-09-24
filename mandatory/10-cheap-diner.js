0563e8ec6987ac7aca71d1f50718fc8b47ac019e
Wiam Nasr <wiamnasr@users.noreply.github.com>
2021-09-24 14:39:39 +0100

    1 remaining yaay!

--- mandatory/10-cheap-diner.js
+++ mandatory/10-cheap-diner.js
@@ -30,7 +30,43 @@ Should give the answer "Nothing :("
 **/
 
 function chooseMeal(mealArray) {
+  let mealToChoose = "";
+  if (mealArray.length == 0) {
+    mealToChoose = "Nothing :(";
+    console.log(mealToChoose);
+  } else if (mealArray.length > 1) {
+    let comparisonArr = [];
+    mealArray.forEach((meal) => comparisonArr.push(meal["price"]));
+    let min = Math.min.apply(Math, comparisonArr);
+    console.log(min);
+
+    let index = comparisonArr.indexOf(min);
+
+    comparisonArr.splice(index, 1);
+    console.log(`comparison Arr: ${comparisonArr}`);
+
+    let minToo = Math.min.apply(Math, comparisonArr);
+    console.log(`min Too: ${minToo}`);
+
+    let filtered = mealArray.filter((meal) => meal["price"] == minToo);
+    console.log(filtered);
+    filtered.forEach((meal) => (mealToChoose = meal["name"]));
+    console.log(mealToChoose);
+  } else {
+    mealArray.forEach((meal) => (mealToChoose = meal["name"]));
+    console.log(mealToChoose);
+  }
+  console.log(`The meal to choose is: ${mealToChoose}`);
 }
+let setOne = [
+  { name: "Burger King", price: 8.99 },
+  { name: "Wingstop", price: 9.99 },
+];
+// { name: "Moe's Southwest Grill", price: 10.99 },
+// { name: "Dunkin' Donuts", price: 8.99 },
+// { name: "Captain D's", price: 13.99 },
+
+chooseMeal(setOne);
 
 /* ======= TESTS - DO MODIFY (!!!) =====
 - To run the tests for this exercise, run `npm test -- --testPathPattern 10-cheap-diner.js`
@@ -39,29 +75,35 @@ function chooseMeal(mealArray) {
 */
 
 test("Meal to select is last", () => {
-  expect(chooseMeal([
-    { name: "Dunkin' Donuts", price: 8.99 },
-    { name: "Captain D's", price: 13.99 },
-    { name: "Moe's Southwest Grill", price: 10.99 },
-  ])).toEqual("Moe's Southwest Grill");
+  expect(
+    chooseMeal([
+      { name: "Dunkin' Donuts", price: 8.99 },
+      { name: "Captain D's", price: 13.99 },
+      { name: "Moe's Southwest Grill", price: 10.99 },
+    ])
+  ).toEqual("Moe's Southwest Grill");
 });
 
 test("Meal to select is first", () => {
-  expect(chooseMeal([
-    { name: "Moe's Southwest Grill", price: 10.99 },
-    { name: "Dunkin' Donuts", price: 8.99 },
-    { name: "Captain D's", price: 13.99 },
-  ])).toEqual("Moe's Southwest Grill");
+  expect(
+    chooseMeal([
+      { name: "Moe's Southwest Grill", price: 10.99 },
+      { name: "Dunkin' Donuts", price: 8.99 },
+      { name: "Captain D's", price: 13.99 },
+    ])
+  ).toEqual("Moe's Southwest Grill");
 });
 
 test("Meal to select is also most expensive", () => {
-  expect(chooseMeal([
-    { name: "Burger King", price: 8.99 },
-    { name: "Wingstop", price: 9.99 },
-  ])).toEqual("Wingstop");
+  expect(
+    chooseMeal([
+      { name: "Burger King", price: 8.99 },
+      { name: "Wingstop", price: 9.99 },
+    ])
+  ).toEqual("Wingstop");
 });
 
-test("Only one meal to select", () => { 
+test("Only one meal to select", () => {
   expect(chooseMeal([{ name: "Subway", price: 8.99 }])).toEqual("Subway");
 });
 
@@ -70,10 +112,12 @@ test("No meals to select", () => {
 });
 
 test("Meal to select is second cheapest, not second most expensive", () => {
-  expect(chooseMeal([
-    { name: "Church's Chicken", price: 8.99 },
-    { name: "Smoothie King", price: 109.99 },
-    { name: "Jason's Deli", price: 22.77 },
-    { name: "Jamba Juice", price: 38.44 },
-  ])).toEqual("Jason's Deli");
+  expect(
+    chooseMeal([
+      { name: "Church's Chicken", price: 8.99 },
+      { name: "Smoothie King", price: 109.99 },
+      { name: "Jason's Deli", price: 22.77 },
+      { name: "Jamba Juice", price: 38.44 },
+    ])
+  ).toEqual("Jason's Deli");
 });
