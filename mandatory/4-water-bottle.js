0b89cf78509e35214840f987a78be2462c434ac2
tenzyns <86624297+tenzyns@users.noreply.github.com>
2021-09-26 14:14:33 +0100

    bottle function complete

--- mandatory/4-water-bottle.js
+++ mandatory/4-water-bottle.js
@@ -24,18 +24,35 @@ let bottle = {
   volume: 0,
   fillUp: function () {
     // calling this function should completely fill your bottle (volume = 100);
+    while (this.volume < 100){
+      this.volume = this.volume + 10;}
   },
   pour: function () {
     // calling this function should increase your bottle volume by 10 units;
+    if (this.volume <= 90){
+    this.volume = this.volume + 10;
+  }
   },
   drink: function () {
     // calling this function should decrease your bottle volume by 10 units;
-  },
+    if (this.volume >= 10){
+    this.volume = this.volume - 10;
+  }
+},
   isFull: function () {
     // this function should return true if your bottle is full;
+    if (this.volume === 100)
+    return true;
+    else {
+    return false;}
   },
   isEmpty: function () {
     // this function should return true if your bottle is empty;
+    if (this.volume === 0)
+    return true;
+    else {
+      return false;
+    }
   },
 };
 
@@ -51,7 +68,7 @@ Extra question:
   Leave your answer below:
 */
 
-// Write you answer to the question here
+// Write you answer to the question here: if we decide to copy the object and assign it to another variable name, then using the object name with the property name in the object method throws an error. 
 
 /*
 Once you have completed your object run the following 
