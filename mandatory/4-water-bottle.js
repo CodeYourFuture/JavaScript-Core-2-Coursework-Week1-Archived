7857b3834b5c13a57b700708b59475032e795ede
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2021-09-29 00:39:45 +0100

    can't run tests on 4
    I'm going to post this problem in slack but until then I'll treat this as complete.

--- mandatory/4-water-bottle.js
+++ mandatory/4-water-bottle.js
@@ -32,18 +32,23 @@ let bottle = {
     // calling this function should increase your bottle volume by 10 units;
     if(100 - this.volume <= 90){
       this.volume += 10;
-    }else if(100 - this.volume > 90){
-      this.volume += (100 - this.volume);
+    // }else if(100 - this.volume > 90){
+    //   this.volume += (100 - this.volume);
     }
   },
   drink: function () {
     // calling this function should decrease your bottle volume by 10 units;
+    if(this.volume >= 10){
+      this.volume -= 10;
+    }
   },
   isFull: function () {
     // this function should return true if your bottle is full;
+    return this.volume === 100;
   },
   isEmpty: function () {
     // this function should return true if your bottle is empty;
+    return this.volume === 0;
   },
 };
 
@@ -60,7 +65,10 @@ Extra question:
 */
 
 // Write you answer to the question here
-
+/*
+  maybe because we may have other instances of this object in use, so we ensure we are effecting just this instance.
+  just what I gathered from doing object oriented game design for a while - I think this is the reason.
+*/
 /*
 Once you have completed your object run the following 
 and see if your answer matches the expected result at the bottom :)
