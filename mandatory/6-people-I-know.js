7254a4700394f66dc9e948d36e0d616cffbc24d9
justsealan <justsealan@gmail.com>
2021-09-30 16:45:36 +0100

    Complete mandatory 6

--- mandatory/6-people-I-know.js
+++ mandatory/6-people-I-know.js
@@ -383,6 +383,11 @@ First, I want you to find all of my friends who are 35 or older.
 */
 
 let thirtyFiveOrOlder = [];
+friends.forEach((friend) => {
+  if (friend.age >= 35) {
+    thirtyFiveOrOlder.push(friend);
+  }
+});
 
 /*
 3) Find the email address
@@ -392,6 +397,11 @@ Next, I want you to find all of my friends who work for "POWERNET" and then stor
 */
 
 let powerNetEmails = [];
+friends.forEach((friend) => {
+  if (friend.company === "POWERNET") {
+    powerNetEmails.push(friend.email);
+  }
+});
 
 /*
 
@@ -406,6 +416,15 @@ This time, I only want the full names ("<firstname> <lastname>") of my friends w
 */
 
 let friendsWhoAreColleaguesOfStacie = [];
+friends.forEach((friend) => {
+  friend.colleagues.forEach((colleague) => {
+    if (colleague.name.includes("Stacie Villarreal")) {
+      friendsWhoAreColleaguesOfStacie.push(
+        friend.name.first + " " + friend.name.last
+      );
+    }
+  });
+});
 /*
 
 5) Find "Multi-tasking" colleagues
@@ -419,6 +438,16 @@ This time, I only want the full names of the people who can multitask
 */
 
 let colleaguesWhoCanMultitask = [];
+friends.forEach((friend) => {
+  friend.colleagues.forEach((colleague) => {
+    colleague.skills.forEach((skill) => {
+      if(skill === "Multi-tasking") {
+    colleaguesWhoCanMultitask.push(colleague.name);
+  }
+  });
+});
+});
+
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 - To run the tests for this exercise, run `npm test -- --testPathPattern 6-people-I-know.js`
@@ -427,8 +456,12 @@ let colleaguesWhoCanMultitask = [];
 */
 
 test("2 - friends that are over 35", () => {
-  expect(thirtyFiveOrOlder.map(({name}) => name.first)).toIncludeSameMembers([
-    "Vilma", "Aisha", "Mitchell", "Sutton", "Jana"
+  expect(thirtyFiveOrOlder.map(({ name }) => name.first)).toIncludeSameMembers([
+    "Vilma",
+    "Aisha",
+    "Mitchell",
+    "Sutton",
+    "Jana",
   ]);
 });
 
@@ -449,10 +482,10 @@ test("4 - friends with Stacie Villarreal as a colleague", () => {
 
 test("5 - colleagues who can multitask", () => {
   expect(colleaguesWhoCanMultitask).toIncludeSameMembers([
-  "Rush May",
-  "Gena Good",
-  "Cunningham Shelton",
-  "Castro Castaneda",
-  "Luz Newton",
+    "Rush May",
+    "Gena Good",
+    "Cunningham Shelton",
+    "Castro Castaneda",
+    "Luz Newton",
   ]);
 });
