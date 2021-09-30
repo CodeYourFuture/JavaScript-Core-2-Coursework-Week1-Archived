7b2cc64a3065b73c97ada73e956d42941e93d48a
justsealan <justsealan@gmail.com>
2021-09-30 19:04:03 +0100

    Complete mandatory 7

--- mandatory/7-recipes.js
+++ mandatory/7-recipes.js
@@ -24,5 +24,61 @@ You should write and log at least 5 recipes
 
 **/
 
-let recipes = {};
+let recipes = [
+  { title: "Mole", servings: 2, ingredients: ["cinnamon", "cumin", "cocoa"] },
+  { title: "Pizza", servings: 2, ingredients: ["dough", "tomato", "cheese"] },
+  { title: "Salad", servings: 2, ingredients: ["lettuce", "tomato", "cheese"] },
+  { title: "Pancakes", servings: 2, ingredients: ["flour", "egg", "milk"] },
+  { title: "Cake", servings: 2, ingredients: ["flour", "egg", "milk"] },
+];
 
+// recipes.forEach((recipe) => {
+//   console.log(recipe.title);
+//   console.log(`Serves: ${recipe.servings}`);
+//   console.log(`Ingredients:`);
+//   recipe.ingredients.forEach((ingredient) => {
+//     console.log(ingredient);
+//   });
+// });
+
+for(let recipe of recipes){
+  console.log(recipe.title);
+    console.log(`Serves: ${recipe.servings}`);
+    console.log(`Ingredients:`);
+    for(ingredient of recipe.ingredients){
+        console.log(ingredient);
+    }
+    console.log("\n");
+}
+
+
+// console.log("*****");
+// console.log(
+//   recipes[0].title +
+//     "\n" +
+//     "Serves: " +
+//     recipes[0].servings +
+//     "\n" +
+//     "Ingredients: " +
+//     "\n" +
+//     recipes[0].ingredients[0] +
+//     "\n" +
+//     recipes[0].ingredients[1] +
+//     "\n" +
+//     recipes[0].ingredients[2]
+// );
+// console.log("*****");
+// console.log(
+//   recipes[1].title +
+//     "\n" +
+//     "Serves: " +
+//     recipes[1].servings +
+//     "\n" +
+//     "Ingredients: " +
+//     "\n" +
+//     recipes[1].ingredients[0] +
+//     "\n" +
+//     recipes[1].ingredients[1] +
+//     "\n" +
+//     recipes[1].ingredients[2]
+// );
