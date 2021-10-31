3deba9cc084c432c2cdd7456545c2571a14f079e
Peter Armstrong <peterarmstrong999@gmail.com>
2021-10-31 16:11:40 +0000

    ex 4

--- mandatory/4-water-bottle.js
+++ mandatory/4-water-bottle.js
@@ -22,20 +22,23 @@ You have to implement the missing features according to the specification.
 // Here is your starting point:
 let bottle = {
   volume: 0,
+  fullCap: 100,
   fillUp: function () {
-    // calling this function should completely fill your bottle (volume = 100);
+    if (!this.isFull()) this.volume = this.fullCap;
   },
   pour: function () {
-    // calling this function should increase your bottle volume by 10 units;
+   const tenDrinkUnits = 10;
+   if (!this.isFull()) this.volume += tenDrinkUnits;
   },
   drink: function () {
-    // calling this function should decrease your bottle volume by 10 units;
+    const tenDrinkUnits = 10;
+    if (!this.isEmpty()) this.volume -= tenDrinkUnits;
   },
   isFull: function () {
-    // this function should return true if your bottle is full;
+    return this.volume === this.fullCap;
   },
   isEmpty: function () {
-    // this function should return true if your bottle is empty;
+    return this.volume === 0;
   },
 };
 
